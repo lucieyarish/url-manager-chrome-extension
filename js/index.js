98ebf6e4ffac54f312c824a6662019355d59ee9e
Lucie Yarish <lucieyarish@hotmail.com>
2023-10-24 08:03:56 +0200

    Feature: delete all btn; do refactoring

--- js/index.js
+++ js/index.js
@@ -1,32 +1,39 @@
 let myLinks = [];
 const inputEl = document.getElementById('input-el');
-const inputBtnEl = document.getElementById('input-btn');
+const saveBtn = document.getElementById('save-btn');
+const deleteBtn = document.getElementById('delete-btn');
 const ulEl = document.getElementById('ul-el');
 
-let linksFromLocalStorage = JSON.parse(localStorage.getItem('myLinks'));
+const linksFromLocalStorage = JSON.parse(localStorage.getItem('myLinks'));
 
 if (linksFromLocalStorage) {
   myLinks = linksFromLocalStorage;
-  renderLinks();
+  render(myLinks);
 }
 
-inputBtnEl.addEventListener('click', function () {
-  myLinks.push(inputEl.value);
-  inputEl.value = '';
-  localStorage.setItem('myLinks', JSON.stringify(myLinks));
-  renderLinks();
-});
-
-function renderLinks() {
+function render(links) {
   let listItems = '';
-  for (i = 0; i < myLinks.length; i++) {
+  for (i = 0; i < links.length; i++) {
     listItems += `
-        <li>
-            <a target="_blank" href="${myLinks[i]}">
-                ${myLinks[i]}
-            </a>
-        </li>
-    `;
+          <li>
+              <a target="_blank" href="${links[i]}">
+                  ${links[i]}
+              </a>
+          </li>
+      `;
   }
   ulEl.innerHTML = listItems;
 }
+
+saveBtn.addEventListener('click', function () {
+  myLinks.push(inputEl.value);
+  inputEl.value = '';
+  localStorage.setItem('myLinks', JSON.stringify(myLinks));
+  render(myLinks);
+});
+
+deleteBtn.addEventListener('dblclick', function () {
+  localStorage.clear();
+  myLinks = [];
+  render(myLinks);
+});
